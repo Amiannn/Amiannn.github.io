eb0455567c2be9c5f1316ae05148673fdced5812
Amiannn <wang.amiannn@gmail.com>
2022-02-16 20:03:06 +0800

    Change demo simple hmm backend api's url

--- demo/Simple-HmmGmm/static/js/lib/api/cfg.js
+++ demo/Simple-HmmGmm/static/js/lib/api/cfg.js
@@ -1,2 +1,3 @@
-const BASE = 'https://140.122.184.163:5555';
+// const BASE = 'https://140.122.184.163:5555';
+const BASE = 'https://simple-hmmgmm-demo.herokuapp.com';
 export const BASE_URL = BASE + '/api';
